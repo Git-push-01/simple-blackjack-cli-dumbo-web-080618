95221e0e2ca20978ffb4d93f08e7e2a5f64f747f
Hugo Escandon <hescandon6@gmail.com>
2018-07-19 16:17:15 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,9 +55,7 @@ def runner
   welcome
   card_total = initial_round
   until card_total > 21
-    hit?(card_total)
-    break
-    display_card_total(card_total)
+    ard_total)
   end
   end_game(card_total)
 end
