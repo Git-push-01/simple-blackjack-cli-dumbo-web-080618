2aea582d5af81442d29fef6f7be564f989abb5e3
Hugo Escandon <hescandon6@gmail.com>
2018-07-19 16:13:27 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -51,14 +51,3 @@ end
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
-def runner
-  # code runner here
-  welcome
-  card_total = initial_round
-  if card_total < 21
-    card_total = hit?(card_total)
-    card_total = display_card_total(card_total)
-  else
-end_game(card_total)
-
-end
